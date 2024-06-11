f7aeb344bcdc79d1175bafb773b310c0cda5dd49
Matt Howell <48911824+MHowell94@users.noreply.github.com>
2024-06-11 10:14:01 +0100

    created xcode project

---
+++ Step Tracker/Step_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Step_TrackerApp.swift
+//  Step Tracker
+//
+//  Created by Matt on 11/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct Step_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
